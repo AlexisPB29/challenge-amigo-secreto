87e651368ad028b467e9b7e2de3112a9797aca7f
AlexisPB29 <alexisperezint29@gmail.com>
2025-09-23 19:28:04 -0500

    limpiar datos

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 // Estado: lista de participantes
 const participantes = [];
+const limpiar = (s) => s.replace(/\s+/g, " ").trim();
